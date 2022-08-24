3533853104ad8525e12bd31148e879510aaa5072
Raslan Amiraslanov <raslan.amiraslanov@gmail.com>
2022-08-24 22:28:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 80,
+    arrowParens: "avoid",
+    proseWrap: "always",
+    quoteProps: "as-needed",
+    semi: true,
+    useTabs: false,
+    tabWidth: 4,
+    trailingComma: "none",
+    bracketSameLine: true
+};
